f97d89c08186267c424d1cb45898d53d1c36e401
Rishad Aufa Harahap <rishadaufaharahap@gmail.com>
2024-07-16 23:08:08 +0700

    Fixing C24

--- routes/users.js
+++ routes/users.js
@@ -2,14 +2,21 @@ var express = require("express");
 var router = express.Router();
 
 const hasSession = require("../helper/util");
-const { getUser, getAddUser, getEditUser, addUser, editUser, deleteUser } = require("../controllers/userControllers");
+const {
+  getUser,
+  getAddUser,
+  getEditUser,
+  addUser,
+  editUser,
+  deleteUser,
+} = require("../controllers/userControllers");
 
 router.get("/", hasSession, getUser);
-router.get("/add", getAddUser)
-router.get("/edit/:id", getEditUser)
-router.get("/delete/:id", deleteUser)
+router.get("/add", hasSession, getAddUser);
+router.get("/edit/:id", hasSession, getEditUser);
+router.get("/delete/:id", hasSession, deleteUser);
 
-router.post("/add", addUser)
-router.post("/edit/:id", editUser)
+router.post("/add", hasSession, addUser);
+router.post("/edit/:id", hasSession, editUser);
 
 module.exports = router;
